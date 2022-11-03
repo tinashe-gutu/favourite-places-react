90278140522521bf71fb5a79bc380758a04e7fb6
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-03 20:57:27 +0000

    add structure for the page to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <h1>Tinashe's Favourite Places</h1>
+      <FavrouitePlace/>
+      <FavrouitePlace/>
+      <FavrouitePlace/>
+      <FavrouitePlace/>
+      <footer>
+        <i>images from google</i>
+      </footer>
+    </>
+  )
 }
 
 export default App;
