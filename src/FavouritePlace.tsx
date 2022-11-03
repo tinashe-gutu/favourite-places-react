cbe8eacf6c2e5cdc6c4c6a2fea8bb5e62c18ee5c
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-03 21:36:53 +0000

    add interface for type Place

--- src/FavouritePlace.tsx
+++ src/FavouritePlace.tsx
@@ -1,10 +1,18 @@
-function FavrouitePlace(props: any): JSX.Element {
+ interface Place {
+    image: string;
+    title: string;
+    place: string[];
+    placeURL: string
+    description: string
+}
+
+function FavrouitePlace(props: Place): JSX.Element {
   return (
     <div>
       <img src={props.image} alt="Place" />
       <h1> {props.title} </h1>
       <h2>
-        {props.place}, {props.country}
+        {props.place.join(',')}
         <a href={props.placeURL}>map link</a>
       </h2>
       <p>{props.description}</p>
