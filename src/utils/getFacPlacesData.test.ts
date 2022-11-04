e82c16f5d8324e463a676835dcf3c9faa258b022
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-04 00:39:18 +0000

    removing template file

--- src/utils/getFacPlacesData.test.ts
+++
@@ -1,7 +0,0 @@
-import { getFavPlacesData } from "./getFavPlacesData";
-
-test("getFavPlacesData returns a string, getFavPlacesData the passed name", () => {
-  expect(getFavPlacesData("World")).toBe("Hello, World!");
-  expect(getFavPlacesData("Richard")).toBe("Hello, Richard!");
-  expect(getFavPlacesData("Academy Scholars")).toBe("Hello, Academy Scholars!");
-});
